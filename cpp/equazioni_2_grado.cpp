75c78b03f84b3ac33248bca187c69d2b91903a37
Matita008 <155455608+Matita008@users.noreply.github.com>
2024-10-24 14:16:48 +0200

    Update cpp

---
+++ cpp/equazioni_2_grado.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std
+
+int main() {
+  int a,b,c;//inizializzo 3 variabili (a,b,c) per memorizzare i coefficienti di un equazione ax^2+bc+c=0
+//Chiedo e leggo i rispettivi coefficienti
+cout<<"Inserisci il coefficiente di x^2\n";
+cin>>a;
+cout<<"\nInserisci il coefficiente di x\n";
+cin>>b;
+cout<<"\nInserisci il termine noto\n";
+cin>>c;
+// non è un'equazione di secondo grado, segnalo e risolvo
+if(a==0)){
+cout<<"il coefficiente di x^2 è nullo(0), pertanto l'equazione è di primo grado\n";//segnalo
+int ris=c/b;//risolvo
+cout<<"Il risultato è "<<ris<<"\n";//restituisco il risultato
+return ris;//termino il programma ritornando il risultato
+}
+int delta=b*b+(4*a*c);
+if(delta<0){//eq. impossibile
+cout<<"L'equazione fornita è impossibile poiché delta ("<<delta<<") è negativo\n";
+}
+else if(delta == 0){//solo un risultato 
+int ris=-b/(2*b);//risolvo
+cout<<"Il risultato è "<<ris<<"\n";//restituisco il risultato
+}
+else{//caso normale, 2 risultati distinti
+int sq=sqrt(delta);
+int ris1=-(b+sq)/(2*a);//risolvo
+int ris2=c;
+cout<<"I risultati sono "<<ris1<<" & "<<ris2<<"\n";//restituisco i risultati
+}
+}
