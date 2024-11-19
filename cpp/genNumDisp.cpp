bca176836c9486df1696b4cc3929809a4c0e0eec
Matita008 <155455608+Matita008@users.noreply.github.com>
2024-11-19 11:38:48 +0100

    Update genNumDisp.cpp
    tab -> spaces

--- cpp/genNumDisp.cpp
+++ cpp/genNumDisp.cpp
@@ -3,25 +3,25 @@
 using namespace std;
 
 int main() {
-  int a, b;
-	
-	cout<<"Inserisci i due estremi positivi in cui generare i numeri separati da uno spazio \" \"\n";
+ int a, b;
+ 
+ cout<<"Inserisci i due estremi positivi in cui generare i numeri separati da uno spazio \" \"\n";
 	cin>>a>>b;
-	while(a<0||b<0){
-	  cout<<"\nAlmeno un numero non e' positivo, riprova!\nInserisci i due estremi in cui generare i numeri separati da uno spazio \" \"\n";
-	  cin>>a>>b;
-  }
-  if(!(a %2)) a++;
-  if(!(b %2)) b--;
-	if(a==b) cout<<a<<"\n\n\nNB: i due estremi inseriti sono identici/contengono solo un valore valido";
+ while(a<0||b<0) {
+  cout<<"\nAlmeno un numero non e' positivo, riprova!\nInserisci i due estremi in cui generare i numeri separati da uno spazio \" \"\n";
+  cin>>a>>b;
+ }
+ if (!(a %2)) a++;
+ if (!(b %2)) b--;
+ if (a==b) cout<<a<<"\n\n\nNB: i due estremi inseriti sono identici/contengono solo un valore valido";
   else {
-  	while (b>=a) {
-  		cout<<a;
+   while (b>=a) {
+   cout<<a;
   		if (a%10==1) cout<<endl;
-  		else cout<<", ";
-  		a+=2;
-	  }
+   else cout<<", ";
+   a+=2;
+   }
   }
   cout<<endl;
-	system("pause");
+ system("pause");
 }
