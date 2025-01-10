aa61fbb3ead959052847ca695cf5e2960ca8d9d5
Matita008 <155455608+Matita008@users.noreply.github.com>
2025-01-10 13:37:01 +0100

    Add files via upload

---
+++ cpp/sommaCifre.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  const int SOMMA = 5;
+  int c1 = 0, c2 = 0, c3 = SOMMA;
+  while(c1 != SOMMA){
+    cout<<c1<<c2<<c3<<", ";
+    //if(c1 == 2) break;
+    if(c3 == 0) {
+        c1++;
+        c2 = 0;
+        c3 = SOMMA - c1;
+    } else {
+      c3--;
+      c2++;
+    }
+  }
+  cout<<SOMMA<<"00";
+  cout<<endl;
+  system("pause");
+}
