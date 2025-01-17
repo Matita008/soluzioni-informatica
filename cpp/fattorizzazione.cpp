8e4301943d48d20a36957c5e339a87bd28f93dc7
Matita008 <155455608+Matita008@users.noreply.github.com>
2025-01-17 11:42:04 +0100

    Optimize fattorizzazione.cpp
    piccola ottimizzazione, è inutile controllare i divisori dall'inizio sempre, tanto se n non è multiplo di 2 la prima volta non lo sarà mai

--- cpp/fattorizzazione.cpp
+++ cpp/fattorizzazione.cpp
@@ -3,24 +3,23 @@
 using namespace std;
 
 int main() {
-  int n, div;
+  int n, div = 2, i;
   cout<<"Inserisci il numero: ";
   cin>>n;
   cout<<"La fattorizzazione di "<<n<<" e': ";
   do {
-    div = 1;
-    for(int i = 2; i * i <= n; i+=2) {
+    for(i = div; i * i <= n; i+=2) {
+      //cout<<"i";//Togli il commento per contare i cicli interni
       if(n %i == 0) {
         div = i;
+        cout<<div<<"*";
+        n/=div;
         break;
       }
       if(i==2) i--;
     }
-    if(div != 1) {
-      cout<<div<<"*";
-      n/=div;
-    }
-  } while (div != 1);
+    //cout<<"e";//Togli il commento per contare i cicli esterni
+  } while (div == i && div*div <= n);
   cout<<n;
   cout<<endl;
   system("pause");
