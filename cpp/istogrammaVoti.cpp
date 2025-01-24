722d43eb0a6b87b38f0da33f9acbb7dd14ad1157
Matita008 <155455608+Matita008@users.noreply.github.com>
2025-01-24 13:16:27 +0100

    Update istogrammaVoti.cpp
    no longer overflow in array

--- cpp/istogrammaVoti.cpp
+++ cpp/istogrammaVoti.cpp
@@ -4,21 +4,26 @@ using namespace std;
 
 int main() {
   const int OF = 18, DIM = 30-OF, FA = 2;
-  int arr[DIM];
+  int voti[DIM];
   int stu;
   cout<<"Quanti studenti sono? ";
   cin>>stu;
   for(int i = 0; i<DIM; i++) {
-    arr[i] = 0;
+    voti[i] = 0;
   }
   cout<<"Inserisci voti ("<<stu<<" studenti): ";
   for(int i = 0; i<stu; i++) {
     int cur;
     cin>>cur;
-    arr[cur-OF]++;
+    if(cur>30||cur<18) {
+      cout<<cur<<" non e' valido, inserisci un numero incluso tra 18 e 30: ";
+      i--;
+      continue;
+    }
+    voti[cur-OF]++;
   }
   for(int i = 0; i<DIM; i++) {
-    int perc = arr[i]/(float)stu*100;
+    int perc = voti[i]/(float)stu*100;
     cout<<endl<<OF+i<<' ';
     for(int a = 0; a < perc; a+=FA) {
       cout<<'*';
